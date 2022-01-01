e956cce0edb6fe46062b3d771be63e99b84ff937
Ahmed Abd Elfatah <chem.ahmedaf@gmail.com>
2022-01-01 16:27:12 +0200

    change color every 5sec

--- main .js
+++ main .js
@@ -1,33 +1,24 @@
 const countDownElm = document.querySelector(".count-down");
 const body = document.querySelector("body");
 let seconds = 5;
-let backgroundColorList = [
-  "red",
-  "blue",
-  "green",
-  "yellow",
-  "orange",
-  "black",
-  "blueviolet",
-  "brown",
-  "chartreuse",
-  "crimson",
-];
-let countDownInternal = setInterval(countDown, 1000);
 
 function countDown() {
   countDownElm.innerHTML = seconds;
-  if (seconds >= 0) {
-    seconds--;
+  if (seconds === 0) {
+    body.style.backgroundColor = getRandomColor();
+    seconds = 5;
   } else {
-    clearInterval(countDownInternal);
-    countDownElm.innerHTML = "background change randomly...";
-    countDownElm.style.color = "white";
-    setInterval(changeBackgroundRandomly, 500);
+    seconds--;
   }
 }
 
-function changeBackgroundRandomly() {
-  let randomIndex = Math.floor(Math.random(backgroundColorList.length) * 10);
-  body.style.backgroundColor = backgroundColorList[randomIndex];
+setInterval(countDown, 1000);
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
